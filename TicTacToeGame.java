e9233b61db5141251a8fd7a566e4e4eecc1dc749
chutki12 <priyankachaudhary2068>
2022-08-06 22:07:30 +0530

    ADDED : This Code For To Create A Tic Tac Toe Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,11 +2,22 @@ package com.bridgelabz.ticTacToeGame;
 
 
 public class TicTacToeGame {
-
-	public static void main(String[] args) {
+	   
+              static char[] board = new char[10];
+	
+              static void createBoard()
+              {
+            	  for(int index = 0 ; index<10 ; index++) {
+            		  board[index] = ' ';
+            		  
+               }
+          }
+	
+              public static void main(String[] args) {
 		
-		System.out.println("Welcome To The Game Of Tic Tac Toe ");
-
-	}
+            	  System.out.println("Welcome To The Game Of Tic Tac Toe ");
+            	  createBoard();
+            	  
+          }
 
 }
