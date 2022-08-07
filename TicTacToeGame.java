1944d401e075f6e95913ef985213c2b80504be21
chutki12 <priyankachaudhary2068>
2022-08-07 10:39:28 +0530

    ADDED: This Code For To Display The Current Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -11,7 +11,7 @@ public class TicTacToeGame {
               public static Scanner scanner = new Scanner(System.in);
           	
               
-              static void createBoard()
+              public static void createBoard()
               {
             	  for(int index = 0 ; index<10 ; index++) {
             		  board[index] = ' ';
@@ -19,7 +19,7 @@ public class TicTacToeGame {
                }
           }
 	          
-              static void getPlayerChoice() {
+              public static void getPlayerChoice() {
          		 System.out.print("select X or O : ");
          		 player = Character.toUpperCase(scanner.next().charAt(0));
          		 
@@ -32,12 +32,22 @@ public class TicTacToeGame {
          		 System.out.println("Computer's Choice Is : " +computer);
          	}
               
+              public static void showBoard() {
+          		System.out.println();
+          		System.out.println("  " + board[1] + "  |  " + board[2] + "   | " + board[3] + "  ");
+          		System.out.println("------------------");
+          		System.out.println("  " + board[4] + "  |  " + board[5] + "   | " + board[6] + "  ");
+          		System.out.println("------------------");
+          		System.out.println("  " + board[7] + "  |  " + board[8] + "   | " + board[9] + "  ");
+          	}
+              
               
               public static void main(String[] args) {
 		
             	  System.out.println("Welcome To The Game Of Tic Tac Toe \n  ");
             	  createBoard();
             	  getPlayerChoice();
+            	  showBoard();
           }
 
 }
