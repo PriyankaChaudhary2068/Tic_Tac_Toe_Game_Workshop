9bc69eb53373d37f2647e4abf3cf440c17ef32a6
chutki12 <priyankachaudhary2068>
2022-08-07 11:07:28 +0530

    ADDED: This Code For To Player Choose The Location From Range 1 To 9 To Make Move

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,16 +2,18 @@ package com.bridgelabz.ticTacToeGame;
 
 import java.util.Scanner;
 
+
 public class TicTacToeGame {
 	   
-             
-              static char[] board = new char[10];
-              static char player;
-              static char computer;
-              public static Scanner scanner = new Scanner(System.in);
+		  public static Scanner scanner = new Scanner(System.in);
+   	
+		  static char[] board = new char[10];
+		  static char player;
+		  static char computer;
+		  private static int playLocation;
           	
-              
-              public static void createBoard()
+             
+          public static void createBoard()
               {
             	  for(int index = 0 ; index<10 ; index++) {
             		  board[index] = ' ';
@@ -19,7 +21,7 @@ public class TicTacToeGame {
                }
           }
 	          
-              public static void getPlayerChoice() {
+            public static void getPlayerChoice() {
          		 System.out.print("select X or O : ");
          		 player = Character.toUpperCase(scanner.next().charAt(0));
          		 
@@ -32,13 +34,27 @@ public class TicTacToeGame {
          		 System.out.println("Computer's Choice Is : " +computer);
          	}
               
-              public static void showBoard() {
+             public static void showBoard() {
           		System.out.println();
           		System.out.println("  " + board[1] + "  |  " + board[2] + "   | " + board[3] + "  ");
           		System.out.println("------------------");
           		System.out.println("  " + board[4] + "  |  " + board[5] + "   | " + board[6] + "  ");
           		System.out.println("------------------");
           		System.out.println("  " + board[7] + "  |  " + board[8] + "   | " + board[9] + "  ");
+          	}
+             
+              
+            public static void userMove() {
+          		System.out.println("Enter Location 1-9 to Make Move");
+          		playLocation = scanner.nextInt();
+          		 
+          		if (playLocation < 10 && playLocation > 0) {
+          			board[playLocation] = player;
+          			showBoard();
+          		}
+          		else {
+          			System.out.println("Invalid Choice");
+          		}
           	}
               
               
@@ -48,6 +64,7 @@ public class TicTacToeGame {
             	  createBoard();
             	  getPlayerChoice();
             	  showBoard();
+            	  userMove();
           }
 
 }
