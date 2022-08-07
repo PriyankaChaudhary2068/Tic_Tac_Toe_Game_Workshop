2c2604aa93d89df4958fbe6200e450a5bb5b70bc
chutki12 <priyankachaudhary2068>
2022-08-07 10:24:37 +0530

    ADDED: This Code For To Player Can Choose Either X or 0 To Play The Game

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,10 +1,16 @@
 package com.bridgelabz.ticTacToeGame;
 
+import java.util.Scanner;
 
 public class TicTacToeGame {
 	   
+             
               static char[] board = new char[10];
-	
+              static char player;
+              static char computer;
+              public static Scanner scanner = new Scanner(System.in);
+          	
+              
               static void createBoard()
               {
             	  for(int index = 0 ; index<10 ; index++) {
@@ -12,12 +18,26 @@ public class TicTacToeGame {
             		  
                }
           }
-	
+	          
+              static void getPlayerChoice() {
+         		 System.out.print("select X or O : ");
+         		 player = Character.toUpperCase(scanner.next().charAt(0));
+         		 
+         		 if (player == 'X')
+         			 computer='O';
+         		 else
+         			 computer ='X';
+         		 
+         		 System.out.println("You Have Selected : " +player);
+         		 System.out.println("Computer's Choice Is : " +computer);
+         	}
+              
+              
               public static void main(String[] args) {
 		
-            	  System.out.println("Welcome To The Game Of Tic Tac Toe ");
+            	  System.out.println("Welcome To The Game Of Tic Tac Toe \n  ");
             	  createBoard();
-            	  
+            	  getPlayerChoice();
           }
 
 }
